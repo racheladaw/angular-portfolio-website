6356b0b02cbc5254f1526c3960e4dc29677b3bfb
Rachel <williamsrachelada@gmail.com>
2020-04-30 14:38:07 -0700

    Declared about-me component in app module and added directive to app component html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
