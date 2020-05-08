1ec49a829ce9a0bbaefb37c4bf117416e0698978
Rachel <williamsrachelada@gmail.com>
2020-05-08 12:10:07 -0700

    Imported components and added routes for each component using angular Routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
