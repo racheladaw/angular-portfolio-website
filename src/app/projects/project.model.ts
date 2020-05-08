1e6cb1ffefa2fb92fe0c90b540dbfb19b73b57cd
Rachel <williamsrachelada@gmail.com>
2020-05-08 13:50:04 -0700

    Changed gitHub path variable to client and backend github path variables.

--- src/app/projects/project.model.ts
+++ src/app/projects/project.model.ts
@@ -3,13 +3,15 @@ export class Project {
   public description: string;
   public imagePath: string;
   public demoPath: string;
-  public githubPath: string;
+  public clientGithubPath: string;
+  public backendGithubPath: string;
 
-  constructor(name: string, desc: string, imagePath: string, demoPath: string, githubPath: string) {
+  constructor(name: string, desc: string, imagePath: string, demoPath: string, clientGithubPath: string, backendGithubPath: string) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
     this.demoPath = demoPath;
-    this.githubPath = githubPath;
+    this.clientGithubPath = clientGithubPath;
+    this.backendGithubPath = backendGithubPath;
   }
 }
